da7df3b37dced2dde41c893301231ed736c8a2fb
yaratavares <yaracriistavares@gmail.com>
2022-01-23 16:52:00 -0300

    redefiniçao da mensagem de erro no login

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-const userLogin = [];
+var userLogin = {};
 const tweets = [];
 
 app.post("/sign-up", (req, res) => {
@@ -14,28 +14,34 @@ app.post("/sign-up", (req, res) => {
   const insertAvatar = req.body.avatar;
 
   if (insertUsername.length === 0 || insertAvatar.length === 0) {
-    res.sendStatus(400);
+    res.status(400).send("Todos os campos são obrigatórios!");
   } else if (!regexImg.test(insertAvatar)) {
-    res.sendStatus(400);
+    res.status(400).send("Formato de imagem não compatível!");
   } else {
-    userLogin.push(req.body);
+    userLogin = {
+      username: insertUsername,
+      avatar: insertAvatar,
+    };
+    console.log(userLogin);
     res.sendStatus(201);
   }
 });
 
 app.post("/tweets", (req, res) => {
   const headerTweet = req.headers;
-  const avatarUser = userLogin.filter(
-    (user) => user.username === headerTweet.user
-  );
-  let tweet = req.body;
-  tweet = {
-    ...tweet,
-    username: avatarUser[0].username,
-    avatar: avatarUser[0].avatar,
-  };
-  tweets.unshift(tweet);
-  res.sendStatus(201);
+  let tweetObject = req.body;
+
+  if (headerTweet.user.length === 0 || tweetObject.tweet.length === 0) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    tweetObject = {
+      ...tweetObject,
+      username: headerTweet.user,
+      avatar: userLogin.avatar,
+    };
+    tweets.unshift(tweetObject);
+    res.sendStatus(201);
+  }
 });
 
 app.get("/tweets/:userName", (req, res) => {
@@ -48,11 +54,12 @@ app.get("/tweets", (req, res) => {
   const idPage = parseInt(req.query["page"]);
   const numberTweets = 10;
   const endTweets = numberTweets * idPage;
+
   if (idPage >= 1) {
     const tweetsForPage = tweets.slice(endTweets - numberTweets, endTweets);
     res.send(tweetsForPage);
   } else {
-    res.send("Informe uma página válida!").status(400);
+    res.status(400).send("Informe uma página válida!");
   }
 });
 
