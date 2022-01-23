f6bee811f7d322ee9889b9220aac81b09c56ebb3
yaratavares <yaracriistavares@gmail.com>
2022-01-23 16:09:09 -0300

    validacion with statuscode

--- app.js
+++ app.js
@@ -9,20 +9,51 @@ const userLogin = [];
 const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-  userLogin.push(req.body);
-  res.sendStatus(201);
+  const regexImg = /(https?:\/\/.*\.(?:png|jpg))/i;
+  const insertUsername = req.body.username;
+  const insertAvatar = req.body.avatar;
+
+  if (insertUsername.length === 0 || insertAvatar.length === 0) {
+    res.sendStatus(400);
+  } else if (!regexImg.test(insertAvatar)) {
+    res.sendStatus(400);
+  } else {
+    userLogin.push(req.body);
+    res.sendStatus(201);
+  }
 });
 
 app.post("/tweets", (req, res) => {
+  const headerTweet = req.headers;
+  const avatarUser = userLogin.filter(
+    (user) => user.username === headerTweet.user
+  );
   let tweet = req.body;
-  tweet = { ...tweet, username: "yara" };
+  tweet = {
+    ...tweet,
+    username: avatarUser[0].username,
+    avatar: avatarUser[0].avatar,
+  };
+  tweets.unshift(tweet);
   res.sendStatus(201);
 });
 
+app.get("/tweets/:userName", (req, res) => {
+  const userName = req.params.userName;
+  const filterTweets = tweets.filter((tweet) => tweet.username === userName);
+  res.send(filterTweets);
+});
+
 app.get("/tweets", (req, res) => {
-  const idPage = req.query["page"];
-  tweets.push(idPage);
-  res.send(tweets);
+  const idPage = parseInt(req.query["page"]);
+  const numberTweets = 10;
+  const endTweets = numberTweets * idPage;
+  if (idPage >= 1) {
+    const tweetsForPage = tweets.slice(endTweets - numberTweets, endTweets);
+    res.send(tweetsForPage);
+  } else {
+    res.send("Informe uma página válida!").status(400);
+  }
 });
 
 app.listen(5000, () => {
